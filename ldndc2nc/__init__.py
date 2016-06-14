cbfd1383ae09ad3689f984e3b95c0bf09021baed
Christian Werner <christian.werner@senckenberg.de>
2016-06-14 18:01:44 +0200

    Fix typo in logging handler

--- ldndc2nc/__init__.py
+++ ldndc2nc/__init__.py
@@ -39,8 +39,7 @@ hCons.setFormatter(lfCons)
 hCons.setLevel(logging.INFO)
 rootLogger.addHandler(hCons)
 
-fileHandler = RotatingFileHandler("{0}/{1}.log".format(logPath, fileName),
-                                  maxBytes=10000)
+hFile = RotatingFileHandler("{0}/{1}.log".format(logPath, fileName), maxBytes=10000)
 hFile.setFormatter(lfFile)
 hFile.setLevel(logging.DEBUG)
 rootLogger.addHandler(hFile)
