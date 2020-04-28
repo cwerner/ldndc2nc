968f75192ce169db6a8edcd1046fd20ac6a064ff
Christian Werner <chr.werner@me.com>
2020-04-28 10:19:26 +0200

    use local version scheme to enable non-tagged scm versions in test pypi

--- setup.py
+++ setup.py
@@ -2,5 +2,12 @@
 
 import setuptools
 
++def version_scheme(version):
++    from setuptools_scm.version import guess_next_dev_version
++
++    version = guess_next_dev_version(version)
++    return version.replace("+", ".")
+
+
 if __name__ == "__main__":
-    setuptools.setup(use_scm_version=True)
+    setuptools.setup(use_scm_version={"version_scheme": version_scheme})
